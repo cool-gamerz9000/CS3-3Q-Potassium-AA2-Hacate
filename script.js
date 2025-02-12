1e07fb6ad576af156186551eae391c6069b209af
cool-gamerz9000 <jpvhacate22@src.pshs.edu.ph>
2025-02-12 21:06:23 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-var Po=initialpopulation;
-var r=rateofgrowth;
-var t=timeinhours;
-function circumference(r) {
-  return parseFloat(r) * 2.0 * Math.PI;
-}
+function populationCalculation(){
+  let initialPopulation = parseFloat(prompt("What is the initial population"));
+  let rateOfGrowth = parseFloat(prompt("What is the rate of growth"));
+  let timeInHours = parseFloat(prompt("What is the time in hours"));
+  let populationCalculation = Math.round(initialPopulation*Math.pow(Math.E,(rateOfGrowth*timeInHours)));
 
-console.log(circumference(4.567));
-document.getElementById('m-m-1').innerHTML = Math.round(s);
+  let region = prompt("What way of region").toUpperCase();
+  let nameOfMonster = prompt("What name of monster").toUpperCase();
+
+
+  document.getElementById("result").innerHTML = "After "+timeInHours+" hours, the population of "+region+" "+nameOfMonster+" has increased to "+populationCalculation+"!";
+}
